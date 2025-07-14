116a17b2dc54bffd6e7b2de097297ca912e8ed1d
Vikas Singh - vissih <officialvissih@gmail.com>
2025-07-14 19:10:55 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="relative w-screen min-h-screen overflow-x-hidden">
+      <Navbar />
+      <section id="home" className="min-h-screen" />
+      <section id="services" className="min-h-screen bg-amber-900" />
+    </div>
+  );
 };
 
 export default App;
